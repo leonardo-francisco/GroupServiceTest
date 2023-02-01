dc7742a64a83f4c3c03d161ef7058f781b8c5099
leonardo-francisco <leonardosfrancisco@gmail.com>
2023-02-01 14:27:38 -0300

    Add project files.

---
+++ GroupService.ConsoleCollatz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GroupService.ConsoleCollatz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior_iteracoes = 0;
+            int numero_maior = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                int iterador = i;
+                int num_iteracoes = 0;
+               
+                if (iterador % 2 == 0)
+                {
+                    iterador = iterador / 2;
+
+                }
+                else
+                {
+
+                    iterador = (iterador * 3) + 1;
+                }
+                num_iteracoes++;
+                if (num_iteracoes > maior_iteracoes)
+                {
+                    maior_iteracoes = num_iteracoes;
+                    numero_maior = i;
+                }
+
+                Console.WriteLine("Percorrendo as interacoes....{0}", i + " / Maior iteracao "+maior_iteracoes+" ,foi o numero "+numero_maior);
+               
+            }
+            Console.WriteLine("Maior iteracao {0} foi o numero {1}", maior_iteracoes, numero_maior);
+        }
+    }
+}
